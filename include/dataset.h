46e6157411ab6532bf607b69cc4d06a67d96c179
Rayyan Jan <janrayyan7@gmail.com>
2026-04-13 22:13:32 +0500

    fixed dataset - added complex polygons, test cases, multiple sizes

--- include/dataset.h
+++ include/dataset.h
@@ -11,5 +11,7 @@ vector<Polygon> loadPolygons(const string& filename);
 vector<Point> loadPoints(const string& filename);
 void generateUniformPoints(int count, double minX, double maxX, double minY, double maxY, const string& filename);
 void generateClusteredPoints(int count, const string& filename);
+void generateMultipleSizes(const string& folder);
+void generateTestCases(const string& filename);
 
 #endif
--- src/dataset.cpp
+++ src/dataset.cpp
@@ -91,22 +91,65 @@ void generateClusteredPoints(int count, const string& filename) {
         return;
     }
 
-    // Define 4 cluster centers (like city centers)
     double centersX[] = {100, 300, 500, 700};
     double centersY[] = {100, 400, 200, 500};
     int numClusters = 4;
 
     for (int i = 0; i < count; i++) {
-        // Pick a random cluster center
         int c = rand() % numClusters;
-
-        // Generate a point near that center with small random offset
         double x = centersX[c] + (rand() / (double)RAND_MAX) * 80 - 40;
         double y = centersY[c] + (rand() / (double)RAND_MAX) * 80 - 40;
-
         file << x << " " << y << "\n";
     }
 
     file.close();
     cout << "Generated " << count << " clustered points to " << filename << endl;
+}
+
+void generateMultipleSizes(const string& folder) {
+    int sizes[] = {1000, 10000, 100000, 1000000};
+    int numSizes = 4;
+
+    for (int i = 0; i < numSizes; i++) {
+        string uniformFile = folder + "/uniform_" + to_string(sizes[i]) + ".txt";
+        string clusteredFile = folder + "/clustered_" + to_string(sizes[i]) + ".txt";
+        generateUniformPoints(sizes[i], 0, 800, 0, 600, uniformFile);
+        generateClusteredPoints(sizes[i], clusteredFile);
+    }
+}
+
+void generateTestCases(const string& filename) {
+    ofstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Error: Could not create file " << filename << endl;
+        return;
+    }
+
+    // Clearly inside polygon 1 (simple square 0,0 to 10,10)
+    file << "5 5\n";
+
+    // Clearly outside all polygons
+    file << "500 500\n";
+
+    // Point on edge of polygon 1
+    file << "5 0\n";
+
+    // Point on vertex of polygon 1
+    file << "0 0\n";
+
+    // Inside polygon 2 outer boundary but inside hole (should be outside)
+    file << "35 35\n";
+
+    // Inside polygon 2 outer boundary and outside hole (should be inside)
+    file << "22 22\n";
+
+    // Inside complex polygon 3
+    file << "150 60\n";
+
+    // Outside complex polygon 3
+    file << "50 50\n";
+
+    file.close();
+    cout << "Generated test cases to " << filename << endl;
 }
